9a77d6066df0d3c06befde4b01003e74e92dd5c9
Aditya <adityadeshpande2010@gmail.com>
2017-07-30 21:06:31 -0400

    Add functions for exception handling demonstration.

--- ExceptionIntro/src/exceptionintro.py
+++ ExceptionIntro/src/exceptionintro.py
@@ -5,7 +5,48 @@ Created on Jul 30, 2017
 
 This program demonstrates the exception handling in python code.
 '''
+def read_file_n_display():
+    print('Working code to read and display the file.')
+    fh = open('sample.txt')
+    for line in fh: print(line.strip())
 
-def main(): pass
+def simple_exception_demo():
+    try:
+        fh = open('xsample.txt') # file handle
+    except:
+        print('Cannot open the file.')
+    else:
+        for line in fh:
+            print(line.strip())
 
+def exception_demo():
+    print('Notice:\nIn try block, the code execution stops at the line where exception occurs. \
+    The further code is not executed.')
+
+    try:
+        fh = open('xsample.txt')
+        for line in fh: print(line.strip())
+    except Exception as e:
+        print('Something happened.')
+        print(e)
+
+def exception_demo_specific_exception():
+    print('Demo for exception handling with specific exception.')
+    try:
+        fh = open('xsample.txt') # file handle
+    except IOError as e:
+        print('Cannot open the file.', e)
+    else:
+        for line in fh:
+            print(line.strip())
+
+def main():
+    simple_exception_demo()
+    print()
+    exception_demo()
+    print()
+    exception_demo_specific_exception()
+    print()
+    read_file_n_display()
+    
 if __name__ == '__main__':main()
